da9e8877cb8eff222025f7ba0e64434afd7157d1
Marco Machado <marco.machado@gmail.com>
2022-03-08 22:48:18 -0300

    Vuetify installation

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark></v-app-bar>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   devServer: { proxy: 'http://localhost:8000' },
   transpileDependencies: true,
+  //transpileDependencies: ['vuetify'],
 });
